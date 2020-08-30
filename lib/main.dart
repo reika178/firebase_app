f88390d23b3aea42cb4bd1d170dc8af8ecaf1890
reika178 <rechiko178@gmail.com>
2020-08-30 22:02:11 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyAuthPage(),
+    );
+  }
+}
+
+class MyAuthPage extends StatefulWidget {
+  @override
+  _MyAuthPageState createState() => _MyAuthPageState();
+}
+
+class _MyAuthPageState extends State<MyAuthPage> {
+  
+  String newUserEmail = "";
+  String newUserPassword = "";
+  String infoText = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: "メールアドレス"),
+                onChanged: (String value) {
+                  setState(() {
+                    newUserEmail = value;
+                  });
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "パスワード（６文字以上）"),
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    newUserPassword = value;
+                  });
+                },
+              ),
+              RaisedButton(
+                onPressed: () async {
+                  try {
+                    final FirebaseAuth auth = FirebaseAuth.instance;
+                    final AuthResult result = 
+                        await auth.createUserWithEmailAndPassword(
+                          email: newUserEmail,
+                          password: newUserPassword,
+                        );
+
+                        final FirebaseUser user = result.user;
+                        setState(() {
+                          infoText = "登録OK: ${user.email}";
+                        });
+                  } catch (e) {
+                    setState(() {
+                      infoText = "登録NG: ${e.message}";
+                    });
+                  }
+                },
+                child: Text("ユーザー登録"),
+              ),
+              Text(infoText)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
